41979f4a3222a9e8b3cd9a8936985bd512cae428
Philip <1016942@student.sae.edu.au>
2020-06-26 16:57:39 +1000

    Shooting(Raycast) + Reloading (Auto)
    + Default GunStats
    + Raycast
    + Reloading (When Magazine empty)

---
+++ Immune Attack/Assets/Scripts/PlayerShoot.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShoot : MonoBehaviour
+{
+
+    //Base Gun Stats//
+    public int bulletsMag = 30;
+    public int bulletsTotal = 300;
+    public float gunfireRate = 0.1f;
+    public float fireTimer;
+    public float gunRange = 100f;
+    public Transform startingshootPoint;
+    public int currentBullets;
+
+    private AudioSource _AudioSource;
+    public AudioClip shootSound;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentBullets = bulletsMag;
+        _AudioSource = GetComponent<AudioSource>();
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if(Input.GetMouseButton(0))
+        {
+            if (currentBullets > 0)
+            {
+                Fire();
+            }
+            else if (currentBullets == 0)
+            {
+                Reload();
+            }
+        }
+
+        if(fireTimer < gunfireRate)
+        {
+            fireTimer += Time.deltaTime;
+        }
+
+    }
+
+    private void Fire()
+    {
+        if(fireTimer < gunfireRate || currentBullets <= 0)
+        {
+            return;
+        }
+
+        RaycastHit hit;
+
+        if (Physics.Raycast(startingshootPoint.position, startingshootPoint.transform.forward, out hit, gunRange))
+        {
+            Debug.Log(hit.transform.name + " was hit!");
+        }
+
+        PlayShootSound();
+
+        currentBullets--;
+        fireTimer = 0.0f;
+    }
+
+    private void PlayShootSound()
+    {
+        _AudioSource.clip = shootSound;
+        _AudioSource.Play();
+    }
+
+    private void Reload()
+    {
+        if(bulletsTotal <= 0)
+        {
+            return;
+        }
+
+        int bulletsRequired = bulletsMag - currentBullets;
+        int bulletsTaken;
+        if (bulletsTotal >= bulletsRequired)
+        {
+            bulletsTaken = bulletsRequired;
+        }
+        else
+        {
+            bulletsTaken = bulletsTotal;
+        }
+
+        bulletsTotal -= bulletsTaken;
+        currentBullets += bulletsTaken;
+    }
+
+}
