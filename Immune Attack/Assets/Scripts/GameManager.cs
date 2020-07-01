2fe819e21da73756d84ad6c3c29ad6d2c43a9730
David <1016359@student.sae.edu.au>
2020-07-01 10:54:38 +1000

    Game Manager
    GameManager script added.
    
    GameManager and its variables can be referenced from any other script via: GameManager.manager

---
+++ Immune Attack/Assets/Scripts/GameManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager manager;
+
+    GameObject player;
+
+    void Awake()
+    {
+        if (manager == null) manager = this;
+        else Destroy(this.gameObject);
+
+        DontDestroyOnLoad(this.gameObject);
+
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+}
