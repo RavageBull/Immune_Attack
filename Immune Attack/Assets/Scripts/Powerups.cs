0d602079d3a90b41e9ea972d240196cbb19dd4e6
Philip <1016942@student.sae.edu.au>
2020-08-07 16:11:08 +1000

    Bladder Attacks + Powerups script

--- Immune Attack/Assets/Scripts/Powerups.cs
+++ Immune Attack/Assets/Scripts/Powerups.cs
@@ -10,6 +10,7 @@ public class Powerups : MonoBehaviour
     public Player stats1;
     public PlayerController stats2;
     public PlayerShoot stats3;
+    //public FirstPersonController stats4;
     
 
     delegate void PowerUpFunctions();
@@ -19,6 +20,13 @@ public class Powerups : MonoBehaviour
         //CreateList
         List<PowerUpFunctions> getPower = new List<PowerUpFunctions>();
         getPower.Add(HealthUp);
+        getPower.Add(MoveSpeed);
+        getPower.Add(FireRate);
+        getPower.Add(DashBuff);
+        getPower.Add(PassiveHPRegen);
+        getPower.Add(TripleJump);
+        getPower.Add(DamageUp);
+        
 
         //Calls functions
         //Can be Random.Range
@@ -45,9 +53,10 @@ public class Powerups : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Player              stats1 = Player1.GetComponent<Player>();
-        PlayerController    stats2 = Player1.GetComponent<PlayerController>();
-        PlayerShoot         stats3 = Player1.GetComponent<PlayerShoot>();
+        Player                  stats1 = Player1.GetComponent<Player>();
+        PlayerController        stats2 = Player1.GetComponent<PlayerController>();
+        PlayerShoot             stats3 = Player1.GetComponent<PlayerShoot>();
+        //FirstPersonController   stats4 = Player1.GetComponent<FirstPersonController>();
     }
 
     // Update is called once per frame
@@ -95,12 +104,19 @@ public class Powerups : MonoBehaviour
 
     void PassiveHPRegen()
     {
-
+        stats1.stats.health += 100;
     }
 
     void TripleJump()
     {
+        //stats4.jumpMax += 1;
 
+        //Need to fix the reference GetComponent since its in a different folder
+    }
+
+    void DamageUp()
+    {
+        stats3.gunATK += 10;
     }
 }
 
