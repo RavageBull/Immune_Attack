ee91832063510493f46e4a1043cc2d09a488e4ff
Philip <1016942@student.sae.edu.au>
2020-09-03 18:54:46 +1000

    Balance Patch 1.2b
    + Brain Boss Hp from 1500 -----> 1250
    + Brain Boss Monster Spawns per attack decreased from 5 ----> 3

--- Immune Attack/Assets/Scripts/Enemies/BrainAttacks.cs
+++ Immune Attack/Assets/Scripts/Enemies/BrainAttacks.cs
@@ -40,7 +40,7 @@ public class BrainAttacks : MonoBehaviour
         StartCoroutine(Strafe());
 
         stats = GetComponent<Stats>();
-        stats.maxHealth = 1500;
+        stats.maxHealth = 1250;
         stats.health = stats.maxHealth;
         stats.damage = 10;
 
@@ -59,7 +59,7 @@ public class BrainAttacks : MonoBehaviour
     {
         if (Input.GetMouseButtonDown(1))
         {
-            ProjectileRingHorizontal();
+            //ProjectileRingHorizontal();
         }
     }
 
